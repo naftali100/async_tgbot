e9bfadddba063642666b1474c337ee9e473d5af0
naftali <naftali100@users.noreply.github.com>
2021-07-14 11:48:43 +0300

    delete message if user not press in new user example

--- examples/restrict_new_users.php
+++ examples/restrict_new_users.php
@@ -12,7 +12,10 @@ $handler = new HandlersHub;
 $handler->on_mew_member(
     func: function(bot_lib\Update $u){
         $u->restrictChatMember($u->chat->id, $u->from->id);
-        $u->reply('please press', Helpers::keyboard([['I am human' => $u->from->id]]));
+        $message = yield $u->reply('please press', Helpers::keyboard([['I am human' => $u->from->id]]));
+        yield Amp\delay(1000 * 60);
+        $message->delete(); // delete the message and ban the user if the user does not press within 1 minute. 
+        $u->ban();
     }
 );
 
