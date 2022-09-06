45cc81e1cad9c600e1c80fd62e007a4d85a99f89
naftali <naftali100@users.noreply.github.com>
2022-09-06 20:12:25 +0300

    add api request test

---
+++ tests/ApiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/basic_update.php';
+
+use Amp\PHPUnit\AsyncTestCase;
+
+final class ApiTest extends AsyncTestCase
+{
+    use UpdateTypes;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->init();
+    }
+
+    public function testFileUpload(){
+        $res = yield $this->private_message->sendPhoto(227774988, __DIR__. '/image.jpg')->array;
+        $this->assertTrue($res['ok']);
+    }
+}
