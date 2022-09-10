e9db85bb677296243fda1fb0ca9a4a2f38e62010
naftali <naftali100@users.noreply.github.com>
2022-09-11 00:16:08 +0300

    add http test

---
+++ tests/HttpTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/basic_update.php';
+
+use Amp\PHPUnit\AsyncTestCase;
+
+final class HttpTest extends AsyncTestCase
+{
+    use UpdateTypes;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->init();
+    }
+
+    public function testArrayBody(){
+        $res = yield $this->private_message->Request('http://example.com', ['data' => 'data'])->promise;
+        $this->assertEquals(200, $res->getStatus());
+    }
+}
