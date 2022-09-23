d6fcfac3973c7726f075c7c3282328551c4cc698
naftali <naftali100@users.noreply.github.com>
2022-09-23 17:25:39 +0300

    initial filters layout

--- src/filter.php
+++ src/filter.php
@@ -1,18 +1,25 @@
 <?php
 
 namespace bot_lib;
-// TODO
-class filter{
-    function test(){
-        print "testtttt!!" . PHP_EOL;
-    }
-    static function message($text, $u){}
-    static function cbq(){}
-    static function service(){}
-    static function file(){}
-    static function user(){}
-    static function chat(){}
-    static function chatType(){}
-    static function regex(){}
-    static function userUpdate(){}
+
+class Filter{
+    // update type filters
+    static function Update($types, $not = false){}
+    static function MessageUpdates($not = false){}
+    static function CbqUpdates($not = false){}
+    static function InlineUpdates($not = false){}
+    static function ServiceUpdates($not = false){}
+    static function FileUpdates($not = false){}
+
+    // filter specific update parts
+    static function Message($text, $not = false){}
+    static function MessageRegex($text, $not = false){}
+    static function Cbq($data, $not = false){}
+    static function User($id, $not = false){}
+    static function Chat($id, $not = false){}
+
+    // type of 
+    static function FileType($type, $not = false){}
+    static function ServiceType($type, $not = false){}
+    static function ChatType($type, $not = false){}
 }
