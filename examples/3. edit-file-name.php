d566f6733e89bca84836c3f6724cc7da3eca6295
naftali <naftali100@users.noreply.github.com>
2022-11-01 15:56:10 +0200

    fix example

--- examples/3. edit-file-name.php
+++ examples/3. edit-file-name.php
@@ -2,17 +2,19 @@
 
 use bot_lib\Config;
 use bot_lib\Handler;
+use bot_lib\Update;
 
-$conf = new Config("123456789:qwertyui");
+$conf = new Config();
+$conf->load('conf.json');
 
 $handler = new Handler;
 
-$handler->on_file(function($u){
+$handler->on_file(function(Update $u){
     $file = yield $u->download()->array; // get decoded json response
     try{
         yield Amp\File\move($file["result"]["file_path"], "new_name");
         $fn = "send" . $u->media["file_type"]; // get the right function to send the file
-        $u->$fn($u->chatId, "new_name", caption: "your renamed file");
+        $u->$fn($u->chat->id, "new_name", caption: "your renamed file");
     }catch(Throwable $e){
         $u->reply("failed to rename");
     }
